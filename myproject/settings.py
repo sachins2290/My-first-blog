2472b710e0721d39b2da0c22e25c7a2f2f66f849
sachins2290 <sachins2290@gmail.com>
2016-01-21 18:20:31 -0800

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,16 +76,6 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'myproject',
-       # 'USER': 'myprojectuser',
-        #'PASSWORD': 'password',
-        #'HOST': 'localhost',
-        #'PORT': '5432',
-    }
-}
 
 
 # Password validation
